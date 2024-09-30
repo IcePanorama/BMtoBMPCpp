d83fa19917476e24139dfc10491cd2adba134984
IcePanorama <thriftedroot@protonmail.com>
2024-09-30 12:49:50 -0400

    Created load_file function.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,33 @@
+#include <format>
+#include <fstream>
 #include <iostream>
+#include <string>
+
+static std::ifstream load_file (const std::string &filename);
 
 int
 main (void)
 {
-  std::cout << "Hello world!\n";
+  const std::string bm_filename = "AUTOGRPH.BM";
+  const std::string pal_filename = "AUTOGRPH.PAL";
+
+  std::ifstream bm_file = load_file (bm_filename);
+  std::ifstream pal_file = load_file (pal_filename);
+
+  bm_file.close ();
+  pal_file.close ();
+
   return 0;
+}
+
+std::ifstream
+load_file (const std::string &filename)
+{
+  std::ifstream file (filename, std::ios::binary);
+  if (!file)
+    {
+      throw std::runtime_error (
+          std::format ("Error opening file, {}.\n", filename));
+    }
+  return file;
 }
