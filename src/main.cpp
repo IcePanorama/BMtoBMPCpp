66e63052f294031e8526a840b145964a286319b8
IcePanorama <thriftedroot@protonmail.com>
2024-09-30 12:52:14 -0400

    Improved error handling.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <format>
 #include <fstream>
 #include <iostream>
+#include <stdexcept>
 #include <string>
 
 static std::ifstream load_file (const std::string &filename);
@@ -11,8 +12,22 @@ main (void)
   const std::string bm_filename = "AUTOGRPH.BM";
   const std::string pal_filename = "AUTOGRPH.PAL";
 
-  std::ifstream bm_file = load_file (bm_filename);
-  std::ifstream pal_file = load_file (pal_filename);
+  std::ifstream bm_file;
+  std::ifstream pal_file;
+
+  try
+    {
+      bm_file = load_file (bm_filename);
+      pal_file = load_file (pal_filename);
+    }
+  catch (std::runtime_error &e)
+    {
+      std::cerr << e.what ();
+      if (bm_file)
+        bm_file.close ();
+      if (pal_file)
+        pal_file.close ();
+    }
 
   bm_file.close ();
   pal_file.close ();
