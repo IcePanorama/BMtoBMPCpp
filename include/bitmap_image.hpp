f3898f7035054a635d1367bb18c97de31e3aa7ea
IcePanorama <thriftedroot@protonmail.com>
2024-10-04 10:39:44 -0400

    Initializing data using the height and width data that we're reading in from the file.

--- include/bitmap_image.hpp
+++ include/bitmap_image.hpp
@@ -2,14 +2,14 @@
 #define _BITMAP_IMAGE_HPP_
 
 #include <cstdint>
+#include <optional>
+#include <vector>
 
 class BitmapImage
 {
-  // std::string filename;
   uint32_t width_;
   uint32_t height_;
-  // TODO: handle this part in a cpp kind of way
-  // uint8_t **data;
+  std::optional<std::vector<std::vector<uint8_t> > > data;
 
 public:
   BitmapImage (uint32_t width, uint32_t height);
--- src/bitmap_image.cpp
+++ src/bitmap_image.cpp
@@ -1,10 +1,10 @@
 #include "bitmap_image.hpp"
 
-#include <format>
-#include <iostream>
+#include <cstdint>
+#include <vector>
 
 BitmapImage::BitmapImage (uint32_t width, uint32_t height)
     : width_ (width), height_ (height)
 {
-  std::cout << std::format ("w: {:d}, h: {:d}\n", this->width_, this->height_);
+  this->data.emplace (this->height_, std::vector<uint8_t> (this->width_));
 }
